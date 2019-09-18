1ac4200bdf5a93d41abc60dd73e324de95e9cbf9
Eugene Haisenka <evheny0@gmail.com>
2019-09-18 19:16:06 +0300

    Improve docker build

--- lib/docker_image_wrapper.rb
+++ lib/docker_image_wrapper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "docker"
+
 class DockerImageWrapper
   def initialize(application_name, repo_full_name)
     @application_name = application_name
@@ -8,7 +10,11 @@ class DockerImageWrapper
   end
 
   def build
-    image = Docker::Image.build_from_dir(@repo_path)
+    image = Docker::Image.build_from_dir(@repo_path) do |v|
+      log = JSON.parse(v)
+      $stdout.puts log["stream"] if log && log.has_key?("stream")
+    end
+
     image.tag("repo" => @image_name, "tag" => @application_name)
     "#{@image_name}:#{@application_name}"
   end
